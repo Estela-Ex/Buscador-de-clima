6c9d4b07631e37a4332c3e3990782d4a10ad7f8f
Estela Extremera <estelaextremeracampos@gmail.com>
2023-10-31 12:13:32 +0100

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,29 @@
+
+export default function Formulario() {
+  return (
+    <div className="contenedor">
+      <form>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input type="text" id="ciudad" name="ciudad" />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">País</label>
+          <select id="pais" name="pais">
+            <option value=""> Seleccione un país</option>
+            <option value="ES">España</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="PE">Perú</option>
+          </select>
+              </div>
+              <input
+                  type="submit"
+                  value="Consultar Clima"
+              />
+      </form>
+    </div>
+  );
+}
---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,16 @@
+import { useState, createContext } from 'react';
+
+const ClimaContext = createContext()
+
+const ClimaProvider = ({ children }) => {
+    return (
+        <ClimaContext.Provider
+        value={{}}
+        >
+            {children}
+        </ClimaContext.Provider>
+    )
+}
+export {
+    ClimaProvider, ClimaContext
+}
